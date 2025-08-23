f06fc555b5f2aa811e5715c6632a223aafeae655
1jani5 <Johnarvinenriquez@gmail.com>
2025-08-23 15:50:28 +0800

    ui done

---
+++ BasicQueuingCashier/CustomerView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicQueuingCashier
+{
+    public partial class CustomerView : Form
+    {
+        public CustomerView()
+        {
+            InitializeComponent();
+            panel1.BackColor = Color.FromArgb(100, Color.WhiteSmoke);
+            panel2.BackColor = Color.FromArgb(100, Color.WhiteSmoke);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void UpdateNowServing(string Number)
+        {
+            label2.Text = Number;
+        }
+    }
+}
